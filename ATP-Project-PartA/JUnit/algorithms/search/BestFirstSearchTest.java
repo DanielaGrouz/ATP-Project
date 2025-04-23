242316ae30b4a7b9f7a22f59e0d64741febdfda3
RinatHadad <hadadri@post.bgu.ac.il>
2025-04-23 12:45:32 +0300

    update Best First Search tests

--- ATP-Project-PartA/JUnit/algorithms/search/BestFirstSearchTest.java
+++ ATP-Project-PartA/JUnit/algorithms/search/BestFirstSearchTest.java
@@ -19,4 +19,20 @@ class BestFirstSearchTest {
         assertThrows(IllegalArgumentException.class,() -> bestFirstSearch.solve(null));
 
     }
+
+    @Test
+    void testProblemWithoutStartOrEnd() {
+        BestFirstSearch bestFirstSearch = new BestFirstSearch();
+
+//        Maze maze = new Maze(3,3);
+//        int[][] matrix = {{0,0,0}, {1,1,0}, {1,1,0}};
+//        Position start = new Position();
+//        Position end = new Position();
+//        maze.setMazeMatrix(matrix);
+
+        //assertThrows(IllegalArgumentException.class,() -> bestFirstSearch.solve(null));
+
+    }
+
+
 }
