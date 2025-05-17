b8131f6e286f93555b9b8b5d6a29d5b540c6f8f4
RinatHadad <hadadri@post.bgu.ac.il>
2025-05-17 13:32:37 +0300

    update MyCompressor & ServerStrategySolveSearchProblem

--- ATP-Project-PartB/src/Server/ServerStrategySolveSearchProblem.java
+++ ATP-Project-PartB/src/Server/ServerStrategySolveSearchProblem.java
@@ -13,10 +13,14 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy{
         try{
             System.out.println("Waiting for object...");
             Maze maze = (Maze) fromClient.readObject();
-            SearchableMaze searchableMaze = new SearchableMaze(maze);
-            ISearchingAlgorithm searcher = new BestFirstSearch();
-            System.out.println("Received dimensions");
-            Solution solution = searcher.solve(searchableMaze);
+            Solution solution = findSolution(maze);
+            if (solution==null){ //the maze hasn't been solved before
+                ISearchingAlgorithm searcher = new BestFirstSearch();
+                SearchableMaze searchableMaze = new SearchableMaze(maze);
+                System.out.println("Received dimensions");
+                solution = searcher.solve(searchableMaze);
+                writeSolutionToFile(maze, solution);
+            }
             toClient.writeObject(solution);
             toClient.flush();
             fromClient.close();
@@ -24,5 +28,25 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy{
         }catch (ClassNotFoundException e){
             throw new IOException("object not found");
         }
+    }
+
+    private Solution findSolution(Maze maze){
+        Solution solution = null;
+
+        //
+
+        return solution;
+    }
+
+    private void writeSolutionToFile(Maze maze, Solution solution) {
+//            int mazeID = maze.getMazeID(); //.toString().hashCode();
+//            String fileName = System.getProperty("java.io.tmpdir") + '\\' + "solution_" + mazeID + ".sol";
+//             File solutionFile = new File(fileName);
+//            if (solutionFile.exists()) {
+//
+//            }
+        //FileOutputStream outStream = new FileOutputStream(fileName);
+        //ObjectOutputStream fileObjectOut = new ObjectOutputStream(outStream);
+
     }
 }
